b1404e4011c120ccb7319c62c95078366951ce9c
Iurii Koval <yuriy.kowal@gmail.com>
2016-05-21 17:53:45 +0300

    added buttons for all pages

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,13 @@
-function getLukesData() {
+function getDetailedInfo(section, itemId) {
+
     var request = new XMLHttpRequest();    
     request.onreadystatechange = function() {
             if(request.readyState == 4 && request.status == 200){
-            document.getElementById("LukesData").innerHTML = request.responseText;
+            document.getElementById("detailedInfo").innerHTML = request.responseText;
         } else {
-            document.getElementById("LukesData").innerHTML = "ready state: " + request.readyState + " status: " + request.status;
+            document.getElementById("detailedInfo").innerHTML = "Loading ... Please wait.";
         }
     }
-    
-    request.open("GET", "http://swapi.co/api/people/1/", true);
+    request.open("GET", "http://swapi.co/api/" + section + "/" + itemId + "/", true);
     request.send();    
 }
